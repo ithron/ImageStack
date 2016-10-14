cc4bd34abaa4293415b4d4a017f2de787b624165
Stefan Reinhold <sre@informatik.uni-kiel.de>
2016-10-14 15:59:00 +0200

    Add type defining header

---
+++ include/ImageStack/Types.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Eigen/Core>
+#include <gsl.h>
+
+namespace ImageStack {
+
+using Size = std::size_t;
+using Size3 = Eigen::Matrix<std::size_t, 3, 1>;
+
+using Index = std::size_t;
+using Index3 = Eigen::Matrix<std::size_t, 3, 1>;
+
+} // namespace ImageStack
