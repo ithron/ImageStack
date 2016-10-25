1dd03f5fa593b1574602f9b6992a2bae972b2556
Stefan Reinhold <sre@informatik.uni-kiel.de>
2016-10-25 11:00:46 +0200

    Add compile time tests for isEigen type trait

--- include/ImageStack/TypeTraits.h
+++ include/ImageStack/TypeTraits.h
@@ -6,9 +6,20 @@
 
 namespace ImageStack {
 
+/// @brief Test if a type is a Eigen matrix type
 template <class T>
 constexpr bool isEigenMatrix_v =
     std::is_base_of<Eigen::MatrixBase<std::decay_t<T>>, std::decay_t<T>>::value;
 
-} // namespace ImageStack
+// Compile time tests
+static_assert(!isEigenMatrix_v<int>, "int is not an Eigen matrix type");
+static_assert(!isEigenMatrix_v<std::array<int, 3>>,
+              "std::array is not an Eigen matrix type");
+static_assert(isEigenMatrix_v<Eigen::Matrix3f>,
+              "Eigen::Matrix3f is an Eigen matrix type");
+static_assert(isEigenMatrix_v<Eigen::Vector2i>,
+              "Eigen::Vector2i is an Eigen matrix type");
+static_assert(isEigenMatrix_v<decltype(Eigen::Vector3i::Zero())>,
+              "Eigen::Vector3i::Zero() is an Eigen matrix type");
 
+} // namespace ImageStack
