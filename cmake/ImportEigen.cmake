3958c56f14280a7678237621804aea73631ba55e
Stefan Reinhold <sre@informatik.uni-kiel.de>
2016-10-27 17:23:40 +0200

    Update cmake import files

--- cmake/ImportEigen.cmake
+++ cmake/ImportEigen.cmake
@@ -1,18 +1,24 @@
 cmake_minimum_required(VERSION 3.2 FATAL_ERROR)
 
-add_library(Eigen INTERFACE IMPORTED)
-if(XCODE)
-  set_property(TARGET Eigen PROPERTY INTERFACE_COMPILE_OPTIONS
-    -isystem$<BUILD_INTERFACE:${DEPENDENCIES_DIR}/eigen>$<INSTALL_INTERFACE:include/eigen>
-  )
-else()
-  set_property(TARGET Eigen PROPERTY INTERFACE_INCLUDE_DIRECTORIES
-      $<BUILD_INTERFACE:${DEPENDENCIES_DIR}/eigen>
-      $<INSTALL_INTERFACE:include/eigen>
-  )
-  set_property(TARGET Eigen PROPERTY INTERFACE_SYSTEM_INCLUDE_DIRECTORIES
-      $<BUILD_INTERFACE:${DEPENDENCIES_DIR}/eigen>
-      $<INSTALL_INTERFACE:include/eigen>
-  )
-endif()
+if(NOT TARGET Eigen)
 
+  add_library(Eigen INTERFACE)
+  if(XCODE)
+    set_property(TARGET Eigen PROPERTY INTERFACE_COMPILE_OPTIONS
+      $<BUILD_INTERFACE:-isystem${DEPENDENCIES_DIR}/eigen>
+    )
+  else()
+    set_property(TARGET Eigen PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+        $<BUILD_INTERFACE:${DEPENDENCIES_DIR}/eigen>
+    )
+    set_property(TARGET Eigen PROPERTY INTERFACE_SYSTEM_INCLUDE_DIRECTORIES
+        $<BUILD_INTERFACE:${DEPENDENCIES_DIR}/eigen>
+    )
+  endif()
+
+  install(DIRECTORY ${DEPENDENCIES_DIR}/eigen/Eigen
+    DESTINATION include/ImageStack
+  )
+  install(TARGETS Eigen EXPORT ImageStackExport)
+
+endif()
