8e7d579b11628c24f17c39a10da4d24477d0cc1a
Stefan Reinhold <stefan@sreinhold.com>
2016-11-21 17:33:33 +0100

    Better configDependencies.sh script

--- configDependencies.sh
+++ configDependencies.sh
@@ -2,23 +2,32 @@
 
 set -e
 
+echo Configuring ImageStack
+
 PROJECT_ROOT=`pwd`
 mkdir -p $PROJECT_ROOT/Dependencies/build
 
-cd Dependencies/build
-
 # Init VolViz
-cd ../VolViz
-./configDependencies.sh
+if [ -d "$PROJECT_ROOT/Dependencies/VolViz/include" ]; then
+  cd $PROJECT_ROOT/Dependencies/VolViz
 
-cd $PROJECT_ROOT/Dependencies/build
+  ln -fs $PROJECT_ROOT/Dependencies/GSL/gsl $PROJECT_ROOT/Dependencies/VolViz/Dependencies/GSL/
+  ln -fs $PROJECT_ROOT/Dependencies/eigen/* $PROJECT_ROOT/Dependencies/VolViz/Dependencies/eigen/
+  ./configDependencies.sh
+else
+  echo "Skip VolViz"
+fi
+
 
 # Build googletest
-mkdir -p googletest
-cd googletest
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_GTEST=On -DBUILD_GMOCK=Off -GXcode ../../googletest
+if [ ! -d "$PROJECT_ROOT/Dependencies/build/googletest" ]; then
+  cd $PROJECT_ROOT/Dependencies/build
+  mkdir -p googletest
+  cd googletest
+  cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_GTEST=On -DBUILD_GMOCK=Off -GXcode ../../googletest
+else
+  echo Skip googletest
+fi
 
 cd $PROJECT_ROOT
 
-ln -s $PROJECT_ROOT/Dependencies/GSL/gsl $PROJECT_ROOT/Dependencies/VolViz/Dependencies/GSL/
-ln -s $PROJECT_ROOT/Dependencies/eigen/* $PROJECT_ROOT/Dependencies/VolViz/Dependencies/eigen/
