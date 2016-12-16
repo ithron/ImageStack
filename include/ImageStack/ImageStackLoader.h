52a10f5a8eafa1d66e65d65a619ec789250c759c
Stefan Reinhold <sre@informatik.uni-kiel.de>
2016-12-16 10:33:00 +0100

    Make IsLoader type trait work with cv or reference qualified types

--- include/ImageStack/ImageStackLoader.h
+++ include/ImageStack/ImageStackLoader.h
@@ -7,7 +7,8 @@ namespace ImageStack {
 template <class Derived> class ImageStackLoaderBase {};
 
 template <class T>
-struct IsLoader : public std::is_base_of<ImageStackLoaderBase<T>, T> {};
+struct IsLoader : public std::is_base_of<ImageStackLoaderBase<std::decay_t<T>>,
+                                         std::decay_t<T>> {};
 
 template <class T> constexpr bool isLoader_v = IsLoader<T>::value;
 
